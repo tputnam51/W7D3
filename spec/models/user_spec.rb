ed6d77985bca35e6ba1df5d691bd9e6ad13c15b6
Travis <tputnam8170@gmail.com>
2021-04-07 16:00:56 -0400

    user_spec, validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,6 +13,12 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
+  subject(:user) do 
+    FactoryBot.build(:user,
+    username: "Johnny Apple",
+    password: "password")
+  end
+
   describe 'validations' do
     it {should validate_presence_of(:username)}
     it {should validate_presence_of(:password_digest)}
@@ -20,4 +26,41 @@ RSpec.describe User, type: :model do
     it {should validate_uniqueness_of(:username)}
     it {should validate_length_of(:password).is_at_least(6)}
   end
+
+  describe '::find_by_credentials' do
+
+    context 'when valid credentials was given' do
+
+      it 'return valid user credentials' do 
+        expect(User.find_by_credentials("Johnny Apple", "password")).to eq(user)
+      end
+
+    end
+
+    context 'when invalid credential was given' do
+      it 'returns nil' do 
+        expect(User.find_by_credentials("Johnny Apple", "wrong_password")).to eq(nil)
+      end
+    end
+
+  end
+
+  describe '#is_password?' do
+
+    context '' do
+
+    end
+
+    
+  end
+
+  describe '#reset_session_token' do
+
+     context '' do
+    
+    end
+
+  end
+ 
+
 end
